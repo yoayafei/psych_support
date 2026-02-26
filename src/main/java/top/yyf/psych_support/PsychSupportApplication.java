409b2ed7a35c37da37a7a6c78e9b1482f99c0f8b
yyyyy <2505114181@qq.com>
2026-02-26 13:47:30 +0800

    first commit

---
+++ src/main/java/top/yyf/psych_support/PsychSupportApplication.java
@@ -0,0 +1,13 @@
+package top.yyf.psych_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/yyf/psych_support/PsychSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yyf.psych_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
